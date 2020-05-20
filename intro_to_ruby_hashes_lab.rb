7b737fc373c1c037e56c8298e6b5e6cd51be5248
leonie crutchfield <leoniecrutchfield@gmail.com>
2020-05-20 21:45:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -37,7 +37,7 @@ def monopoly_with_third_tier
    
   def monopoly_with_fourth_tier
     
-     monopoly = {:railroads=>
+     monopoly  {:railroads=>
      {
     :pieces=> 4,
     :rent_in_dollars=>
